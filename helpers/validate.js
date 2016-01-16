da6159dc2c6036ece774896004b6f66e5f83a08c
Daniel Schougaard <danielschougaard@gmail.com>
2016-01-16 11:22:39 +0100

    Changed how some validators work, added others

--- helpers/validate.js
+++ helpers/validate.js
@@ -1,5 +1,9 @@
 const validator = require('validator');
 
+exports.json = function(json){
+	return ( json !== undefined && id !== '' && validator.isJSON(json) );
+}
+
 exports.username = function(username) {
 	return ( username !== undefined && username !== '' && validator.isAlphanumeric(username) );
 }
@@ -13,9 +17,18 @@ exports.privateKey = function(privateKey){
 }
 
 exports.publicKey = function(publicKey){
-	return (publicKey !== undefined && publicKey !== '' && this.base64(publicKey) ); 
+	return ( publicKey !== undefined && publicKey !== '' && this.base64(publicKey) ); 
 }
 
 exports.base64 = function(base64){
-	return base64.match(/^([A-Za-z0-9+\/]{4})*([A-Za-z0-9+\/]{4}|[A-Za-z0-9+\/]{3}=|[A-Za-z0-9+\/]{2}==)$/);
+	//return base64.match(/^([A-Za-z0-9+\/]{4})*([A-Za-z0-9+\/]{4}|[A-Za-z0-9+\/]{3}=|[A-Za-z0-9+\/]{2}==)$/);
+	return ( validator.isBase64(base64) );
+}
+
+exports.ID = function(id){
+	return ( id !== undefined && id !== '' && validator.isInt(id) );
+}
+
+exports.string = function(str){
+	return ( str !== undefined && str !== '' )
 }
