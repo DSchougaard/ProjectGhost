552a88c00dcce649d59f9ef21177a94e37f3d6f9
Daniel Schougaard <danielschougaard@gmail.com>
2016-01-16 01:34:57 +0100

    Auth now uses bunyan for logging!

--- routes/auth.js
+++ routes/auth.js
@@ -9,18 +9,19 @@ const validator			= require('validator');
 const authHelpers 		= require(__base + '/helpers/authHelpers.js');
 
 
-module.exports = function(server, knex){
+module.exports = function(server, knex, log){
 	server.post('/api/auth/login', function(req, res, next){
+		log.info({ method: 'POST', path: '/api/auth/login', payload: req.body.username });
 
 		// Filter bad requests based on the username
 		if( req.body.username === undefined || req.body.username === '' || !validator.isAlphanumeric(req.body.username) ){
-			console.log("POST /api/auth/login Missing username");
+			log.debug({ method: 'POST', path: '/api/auth/login', payload: req.body.username, message: 'Missing username'});
 			return next(new restify.errors.BadRequestError("Incomplete request: Missing username"));
 		}
 
 		// Filter bad requests based on the password
 		if( req.body.password === undefined || req.body.password === '' ){
-			console.log("POST /api/auth/login Missing password");
+			log.debug({ method: 'POST', path: '/api/auth/login', payload: req.body.username, message: 'Missing password'});
 			return next(new restify.errors.BadRequestError("Incomplete request: Missing password"));
 		}
 
@@ -28,31 +29,28 @@ module.exports = function(server, knex){
 		.then(function(rows){
 			if( rows.length == 0 ){
 				// No user was found with that username.
-				console.log("POST /api/auth/login Wrong Username");
+				log.debug({ method: 'POST', path: '/api/auth/login', payload: req.body.username, message: 'Wrong Username'});
 				return next(new restify.errors.UnauthorizedError('Wrong login credentials'));
 			}
 			if( rows.length > 1 ){
-				console.log("POST /api/auth/login Something went horribly wrong...");
+				log.error({ method: 'POST', path: '/api/auth/login', payload: req.body.username, message: 'Several user IDs found for same username. Fatal error.'});
 				return next(new restify.errors.InternalServerError());
 			}
 
 			bcrypt.hash(req.body.password, rows[0].salt, function(err, hash){
 				if(err){
-					console.log("POST /api/auth/login BCrypt error.");
+					log.error({ method: 'POST', path: '/api/auth/login', payload: req.body.username, message: 'BCrypt error', error: err});
 					return next( new restify.errors.InternalServerError() );
 				}
 				req.body.password = '';
 
-				if(err){
-					console.log("POST /api/auth/login Argon2 encryption error.\n%j", err);
-					return next(new restify.errors.InternalServerError());
-				}
 				if( hash !== rows[0].password ){
 					// Login failed
+					log.info({ method: 'POST', path: '/api/auth/login', payload: req.body.username, message: 'Login failed'});
 					return next(new restify.errors.UnauthorizedError('Wrong login credentials'));
 				}
 
-				console.log('Authenticated user %s.', rows[0].username);
+				log.info({ method: 'POST', path: '/api/auth/login', payload: req.body.username, message: 'Login successful'});
 
 				// Login Succeeded
 				res.send(200, {token: authHelpers.createJWT(rows[0]) });
