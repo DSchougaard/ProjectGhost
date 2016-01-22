c7fc685d2432e6255c31d5faa6c14828fb7fe8f7
Daniel Schougaard <danielschougaard@gmail.com>
2016-01-23 00:25:55 +0100

    I think the database.js finally works?

--- database.js
+++ database.js
@@ -1,3 +1,42 @@
+const config = require(__base + 'config.json');
+
+
+var opts = {
+	database : typeof config.database !== undefined ? config.database : 'sqlite'
+}
+
+if( config.database === 'sqlite' ){
+	opts.connection = config.sqlite_connection;
+}
+
+// Unittest DB Override
+if( process.env.NODE_ENV === 'test' ){
+	opts.connection.filename = './unittest.sqlite';
+}
+
+var knex = require('knex')({
+	client: opts.database,
+	connection : opts.connection
+});
+
+
+module.exports = knex;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*module.exports.connect = function(opts){
 	knex = require('knex')({
@@ -10,19 +49,28 @@
 module.exports.get = function(){
 	return knex;
 }*/
+/*
+var knex = undefined;
+module.exports.connect = function(opts){
+	knex = require('knex')(opts);
+	return knex;
+}
 
-
+module.exports.get = function(){
+	return knex;
+}
+*/
+/*
 module.exports = function(opts){
-	var knex = undefined;
 
 	if( opts === undefined ){
+		console.log("Grabbing KNEX");
 		return knex;
 	}
 
-	knex = require('knex')({
-		client: opts.database,
-		connection: opts.connection
-	});
+	knex = require('knex')(opts);
+	console.log("Creating new KNEX: %j", opts);
 	return knex;
 
 }
+*/
