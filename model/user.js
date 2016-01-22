53a35a8e0ff3dee966cf80c063557042eaa7d44f
Daniel Schougaard <danielschougaard@gmail.com>
2016-01-22 14:10:34 +0100

    Model/User now has #create and #find methods

--- model/user.js
+++ model/user.js
@@ -1,6 +1,12 @@
 'use strict'
-const Promise = require('bluebird');
+const Promise 	= require('bluebird');
+const bcrypt 	= require('bcrypt');
+const genSalt 	= Promise.promisify(bcrypt.genSalt);
+const hash 		= Promise.promisify(bcrypt.hash);
+
+
 var schemagic = require('schemagic');
+
 
 //const userSchema = require(__base + 'schemas/userSchema.js');
 
@@ -14,23 +20,43 @@ const OperationalError 		= Promise.OperationalError;
 
 module.exports = class User{
 	constructor(data){
-		this.init 	= false;
-		this.user 	= {};
-
-		this.user 	= data;
+		this.id 		= data.id;
+		this.username 	= data.username;
+		this.isAdmin 	= data.isAdmin;
+		this.password 	= data.password;
+		this.salt 		= data.salt;
+		this.privatekey = data.privatekey;
+		this.publickey 	= data.publickey;
 	}
 
 	static create(data){
-		var validate = schemagic.userSchema.validate(data);
+		var validate = schemagic.userInput.validate(data);
 		if( !validate.valid ){
-			console.log("Invalid! %j", validate);
 			return new Promise.reject( new ValidationError(validate.errors[0].message, validate.errors[0].property) );
 		}
 
-		return new Promise.resolve(new User(data));
+		return genSalt()
+		.then(hash.bind(null, data.password))
+		.then(function(hash){
+			data.password = hash;
+			data.salt = hash.substring(0, 29);
+
+			return knex
+			 	.insert(data)
+			 	.into('users');
+		})
+		.then(function(id){
+			data.id = id[0];
+			return new Promise.resolve( new User(data) );
+		});
 	}
 
 	static find(id){
+
+		if( typeof id !== 'number' ){
+			return new Promise.reject( new ValidationError('wrong type', 'id') )
+		}
+
 		return knex
 			.select()
 			.from('users')
@@ -43,14 +69,24 @@ module.exports = class User{
 					return new Promise.reject( new OperationalError('Catatrophic database error. Multiple users with same ID found.') );
 				}
 
-				var validate = schemagic.userSchema.validate(rows[0]);
+				if( typeof rows[0].isAdmin === 'number' ){
+					rows[0].isAdmin = true;
+				}
+				var validate = schemagic.user.validate(rows[0]);
 				if( !validate.valid ){
-					//return new Promise.reject( new ValidationError(validate.message, validate.property) );
-					throw new OperationalError("Validation Error!");
-					//throw new ValidationError(validate.message, validate.property);
+					return new Promise.reject( new ValidationError(validate.errors[0].message, validate.errors[0].property) );
 				}
 
 				return new Promise.resolve(new User(rows[0]));
 			});
+	}
+
+
+	update(fields){
+
+	}
+
+	del(){
+
 	}
 }
