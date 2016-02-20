5506dfc907a17c51f440dcc6ed39227c4f5cbd62
Daniel Schougaard <danielschougaard@gmail.com>
2016-02-20 12:11:54 +0100

    Index now uses minified bundled version for vendors and app.

---
+++ gulpfile.js
@@ -0,0 +1,74 @@
+var gulp 			= require('gulp');
+var concat 			= require('gulp-concat');
+var watch 			= require('gulp-watch');
+var htmlv 			= require('gulp-html-validator');
+var htmlhint 		= require("gulp-htmlhint");
+var mainBowerFiles 	= require('gulp-main-bower-files');
+var gulpFilter 		= require('gulp-filter');
+var uglify 			= require('gulp-uglify')
+var sourcemaps 		= require('gulp-sourcemaps');
+
+gulp.task('concat', function() {
+  return gulp.src(['./public/js/**/*.js', './public/toolbar/*.js'])
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('./public/'));
+});
+
+
+var dirs = ['./public/app/**/*.js'] 
+gulp.task('auto-concat', function(cb){
+	watch(dirs, function(){
+		console.log(new Date());
+
+		gulp.src(dirs)
+		.pipe(sourcemaps.init())
+			.pipe(concat('bundle.js'))
+		.pipe(sourcemaps.write())
+	    .pipe(gulp.dest('./public/'));
+
+	})
+});
+
+
+var t = {
+	overrides: {
+		forge: {
+			main: [
+				'./js/forge.bundle.js'
+				]
+			}
+		}
+	}
+
+
+gulp.task('vendor-concat', function(){
+    var filter = gulpFilter('**/*.js');
+
+
+
+	return gulp.src('./bower.json')
+		.pipe(mainBowerFiles(t))
+		.pipe(filter)
+		.pipe(sourcemaps.unit())
+			.pipe(concat('vendor.js'))
+		.pipe(sourcemaps.write())
+		.pipe(uglify())
+		.pipe(gulp.dest('./public/'))
+});
+
+
+
+gulp.task('validate', function(){
+	return gulp.src(['./public/app/**/*.html'])
+	.pipe(htmlhint())
+  	.pipe(htmlhint.reporter("htmlhint-stylish"))
+})
+
+
+
+
+
+
+
+
+gulp.task('default', ['concat', 'validate']);
