1890e9acd164d32677fff21594e35048916a5adc
Daniel Schougaard <danielschougaard@gmail.com>
2016-02-22 17:46:33 +0100

    Removed unneccesary code

--- public/app/password-list/password-list.controller.js
+++ public/app/password-list/password-list.controller.js
@@ -31,86 +31,11 @@
 		// UI show/hide statusses
 		self.isVisible = isVisible;
 
-		// User Menu Controls
-		self.selectMenu = selectMenu;
-
-
-
-		// Test Data
-		self.selection = "";
-		self.treeStructure = [
-			{
-				title: 'Group 1',
-				type: 'node',
-				children: [
-					{
-						title: 'Other Title',
-						type: 'leaf'
-					},
-					{
-						title: 'Other Title 2',
-						type: 'leaf'
-					}
-				]
-			},
-			{
-				title: 'Group 2',
-				type: 'node',
-				children: [
-					{
-						title: 'Another Title',
-						type: 'leaf'
-					},
-					{
-						title: 'Another Title 2',
-						type: 'leaf'
-					}
-				]
-			},
-			{
-				title: 'This is interesting',
-				type: 'node',
-				children: [
-					{
-						title: 'A little more interesting',
-						type: 'node',
-						children:[
-							{
-								title: 'Hmmmmmm',
-								type: 'leaf',
-								selection: 'Bambus!'
-							}
-						]
-					}
-				]
-			},
-			{
-				title: 'Node Without children and a very very very very very very very looooooong title',
-				'type': 'node',
-				children: []
-			}
-		];
-
 		// Watch for change, need this for initial load
 		$scope.$on('passwords', function(res){
 			self.entries = PasswordService.passwords;
 		});
 
-		function selectMenu(index){
-			switch(index){
-				case 0:
-					console.log("Going to User.Edit");
-					$state.go('user');
-					break;
-				case 1:
-					$auth.logout();
-					$state.transitionTo("login");
-					break;
-				default:
-					console.log('Invalid selection');
-					break;
-			};
-		}
 
 		// Method for determining wheter or not a field is shown	
 		function isVisible(value){
