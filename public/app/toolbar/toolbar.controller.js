61095cee58f5fe240211569fa8c2e7cada2b48eb
Daniel Schougaard <danielschougaard@gmail.com>
2016-02-26 12:27:56 +0100

    Nested pages has back button

--- public/app/toolbar/toolbar.controller.js
+++ public/app/toolbar/toolbar.controller.js
@@ -3,14 +3,27 @@
 	.module('ghost')
 	.controller('ToolBarController', ToolBarController);
 
-	function ToolBarController($mdSidenav){
+	function ToolBarController($mdSidenav, $state){
 		console.log("ToolBarControlelr initiated");
 
 		var self = this;
-		// Methods
-		self.menu = menu;
+		self.menu = false;
+		
+		if( $state.is('home') ){
+			console.log("Menu it is...");
+			self.menu = true;
+		}
 
-		function menu(){
+		// Methods
+		self.toggleMenu = toggleMenu;
+		self.back = back;
+
+		function back(){
+			$state.transitionTo('home');
+		}
+ 
+
+		function toggleMenu(){
 			console.log("Toggling sidenav");
 			$mdSidenav('left').toggle();
 		}
