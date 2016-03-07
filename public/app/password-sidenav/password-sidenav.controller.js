5fb49f8decf77fc1494a33f2a8c4e5c494e8df8c
Daniel Schougaard <danielschougaard@gmail.com>
2016-03-07 17:45:25 +0100

    Misc cleanup

--- public/app/password-sidenav/password-sidenav.controller.js
+++ public/app/password-sidenav/password-sidenav.controller.js
@@ -3,7 +3,6 @@
 	.module('ghost')
 	.controller('PasswordSideNavController', PasswordSideNavController);
 
-
 	function PasswordSideNavController($rootScope, $state, $http, $auth, AuthorizationService, CategoryService, $timeout, $mdDialog){
 		var self = this;
 
@@ -25,7 +24,7 @@
 		self.category 		= {};
 		self.category.add 	= addCategory
 
-
+		// Fetch content of category tree
 		CategoryService.structure()
 		.then(function(structure){
 			console.log("%j", structure);
@@ -39,31 +38,7 @@
 			self.categories.push(personalPasswords);
 		})
 
-		/*
-		fetch();
-		*/
 		// Methods
-		function fetch(){
-			$http({
-				method: 'GET',
-				url: '/api/users/' + $auth.getPayload().uid + '/categories'
-			})
-			.then(function(res){
-				//self.categories = createStructure(res.data);
-				var personalPasswords = {
-					title: 'Personal Passwords',
-					id: null,
-					children: createStructure(res.data),
-					initial:true
-				};
-				self.categories = [];
-				self.categories.push(personalPasswords);
-
-			}, function(err){
-				console.error(err);
-			})
-		}
-
 		function addCategory(ev){
 			$mdDialog.show({
 				controller: 'CategoryController',
@@ -80,9 +55,7 @@
 			}, function() {
 				console.log("Cancel");
 			});
-
 		}
-
 
 		function selectMenu(item){
 			switch(item){ 
@@ -103,43 +76,9 @@
 			};
 		}
 
-		/*
-		function createStructure(categories){
-			var map = {};
-			var structure = [];
-
-			for( var i = 0 ; i < categories.length ; i++ ){
-				map[categories[i].id] = categories[i];
-			}
-
-			for( var i = 0 ; i < categories.length ; i++ ){
-				// Create children list on the category
-				var category = categories[i];
-
-				// Append Values for the tree;
-				category.type = 'node';
-
-				// Create array in parent
-				var parent = map[category.parent];
-				if( parent !== undefined && parent !== null && category.parent !== category.id){
-					// Category has a parent1
-					if( parent.children === undefined ){
-						parent.children = [];
-					}
-					parent.children.push(category);
-				}else{
-					// Category does not have a parent.
-					structure.push(category);
-				}
-			}
-
-			return structure;
-		}
-		*/
 		function treeSelect(selection){
 			$rootScope.$broadcast('category', selection);
 		}
-
 	};
 })();
 
