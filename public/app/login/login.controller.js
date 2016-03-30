e1252b2cdf7c49589bdf8f09ba46d8515031becb
Daniel Schougaard <danielschougaard@gmail.com>
2016-03-30 14:46:12 +0200

    Login now actually handles 2FA

--- public/app/login/login.controller.js
+++ public/app/login/login.controller.js
@@ -4,7 +4,7 @@
 		.module('ghost')
 		.controller('loginController', LoginController);
 
-	function LoginController($scope, $auth, $location, $state, $mdToast){
+	function LoginController($scope, $auth, $mdDialog, $location, $state, $mdToast){
 		var self = this;
 
 		// Literals
@@ -22,15 +22,38 @@
 				$state.transitionTo("home");
 			})
 			.catch(function(err){
+				console.log("%j", err)
 
-				var error = err.data.message ? err.data.message : 'Unidentified error loggin in';
+				if( err.status === 403 ){
+					// Two-Factor Token is Required
+					var token = $mdDialog.prompt()
+					.title('Please enter the verification code')
+					.placeholder('Code')
+					.ariaLabel('verification code')
+					.ok('OK!')
+					.cancel('No thanks.');
 
-				$mdToast.show(
-					$mdToast.simple()
-					.textContent(error)
-					.position("top right")
-					.hideDelay(3000)
-				);
+
+					$mdDialog.show(token)
+					.then(function(result){
+						self.user.twoFactorToken = result;
+						return auth();
+					});
+				}else{
+					// Make sure to wip 2FA token
+					self.user.twoFactorToken = undefined;
+
+					// Just plain old wrong credentials
+					var error = err.data.message ? err.data.message : 'Unidentified error loggin in';
+
+					$mdToast.show(
+						$mdToast.simple()
+						.textContent(error)
+						.position("top right")
+						.hideDelay(3000)
+					);		
+				}
+
 			});
 		}
 
