9de2e2524edc87c480a512e6903917417584818f
Daniel Schougaard <danielschougaard@gmail.com>
2016-04-03 15:34:56 +0200

    Fixed naming errors in schemas

--- schemas/sharedPassword.js
+++ schemas/sharedPassword.js
@@ -16,7 +16,7 @@ module.exports = {
 			"pattern":/[0-9]+/,
 			"example":1
 		},
-		"owner_origin":{
+		"origin_owner":{
 			"description": "User ID of the user owning the original Password Entry",
 			"required":true,
 			"type": ["number", "string"],
@@ -29,7 +29,7 @@ module.exports = {
 			"type": "string",
 			"pattern":/^([A-Za-z0-9+\/]{4})*([A-Za-z0-9+\/]{4}|[A-Za-z0-9+\/]{3}=|[A-Za-z0-9+\/]{2}==)$/
 		},
-		"owner_origin":{
+		"origin_password":{
 			"description": "Password ID of the origin, of the shared password",
 			"required":true,
 			"type": ["number", "string"],
