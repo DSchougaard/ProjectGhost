e674b037a1468ed432ecb73eb0287e08b0a22541
Daniel Schougaard <danielschougaard@gmail.com>
2016-06-20 14:04:02 +0200

    Argon install script works on mac again

--- argon-install.sh
+++ argon-install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 git clone https://github.com/ranisalt/node-argon2.git node_modules/argon2
 cd node_modules/argon2
-git checkout bluebird
 git submodule update --init
-npm install
-node-gyp rebuild
+CXX=g++-5 npm install
+node-gyp build
